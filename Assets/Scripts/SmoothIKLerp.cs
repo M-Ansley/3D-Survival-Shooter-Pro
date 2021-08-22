9792f3e17e9d46ce92b88c0b334870a3d3ac3ddb
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-08-22 10:08:08 +0100

    IK Rigging Hands

---
+++ Assets/Scripts/SmoothIKLerp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothIKLerp : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField] private float _speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = target.transform.position;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * _speed);
+        transform.rotation = Quaternion.Euler(target.transform.rotation.eulerAngles);
+    }
+}
