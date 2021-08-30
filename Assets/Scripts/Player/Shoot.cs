4125f04c4bc42f5f81bf6c624a610257859f7e1d
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-08-30 16:26:57 +0100

    Minor Adjustments

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -9,12 +9,14 @@ public class Shoot : MonoBehaviour
     [SerializeField] private int _damageAmount = 5;
 
     [SerializeField] private GameObject[] _bloodSpatterPrefabs;
-    
+
+    [SerializeField] private LayerMask _layer;
+
     void Start()
     {
         _mainCamera = Camera.main;
     }
-    
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -28,21 +30,38 @@ public class Shoot : MonoBehaviour
         RaycastHit hit;
         Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit, 50f, _layer, QueryTriggerInteraction.Ignore))
         {
+            Debug.Log("Passed through");
             Debug.Log(hit.collider.name);
             Health targetHealth = hit.collider.gameObject.GetComponent<Health>();
             if (targetHealth != null)
             {
-                InstantiateBloodSpatter(hit);                
+                InstantiateBloodSpatter(hit);
                 targetHealth.Damage(_damageAmount);
             }
+        }      
+        else
+        {
+            Debug.Log("Ignored");
         }
+
+        //if (Physics.Raycast(ray, out hit))
+        //{
+        //    Debug.Log(hit.collider.name);
+        //    Health targetHealth = hit.collider.gameObject.GetComponent<Health>();
+        //    if (targetHealth != null)
+        //    {
+        //        InstantiateBloodSpatter(hit);
+        //        targetHealth.Damage(_damageAmount);
+        //    }
+        //}
     }
 
     private void InstantiateBloodSpatter(RaycastHit hit)
     {
-        GameObject spatter = Instantiate(_bloodSpatterPrefabs[Random.Range(0, _bloodSpatterPrefabs.Length)], hit.point + (hit.normal * 0.01f), Quaternion.LookRotation(hit.normal));
+        GameObject _spatterPrefab = _bloodSpatterPrefabs[Random.Range(0, _bloodSpatterPrefabs.Length)];
+        GameObject spatter = Instantiate(_spatterPrefab, hit.point + (hit.normal * 0.01f), Quaternion.LookRotation(hit.normal));
         spatter.transform.localScale = spatter.transform.localScale * 0.5f;
         spatter.transform.parent = hit.transform;
     }
--- Assets/Scripts/SmoothIKLerp.cs
+++ Assets/Scripts/SmoothIKLerp.cs
@@ -7,17 +7,9 @@ public class SmoothIKLerp : MonoBehaviour
     public Transform target;
     [SerializeField] private float _speed = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 targetPosition = target.transform.position;
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * _speed);
+        transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * _speed);
         transform.rotation = Quaternion.Euler(target.transform.rotation.eulerAngles);
     }
 }
